0f9c2cc361001f7901411ad163e089d4a0870cc3
Armougom <y.armougom@gmail.com>
2019-10-02 11:03:43 +0200

    premier commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('yannick'));
+console.log(chalk.red('salim'));
+console.log(chalk.magentaBright('andrea'));
+console.log(chalk.green('mohamed'));
+
